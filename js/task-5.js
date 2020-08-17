efac806ddfcbb10f3106d828cb4f6c15a3a9f058
AnhelinaDiakova <Gelja4ka@gmail.com>
2020-08-17 21:35:23 +0300

    Добавляет 6 задачу (

--- js/task-5.js
+++ js/task-5.js
@@ -68,3 +68,29 @@ console.log(message);
 //если countryName равно "Чили"
 // то значение message будет равно
 // 'В выбранную страну доставка недоступна.'
+
+/*
+ * Напиши скрипт поиска самого маленького числа и самого большого в массиве,
+ * при условии что числа уникальные (не повторяются).
+ */
+
+// const numbers = [51, 18, 13, 24, 7, 85, 19];
+// let smallestNumber = numbers[0];
+
+// for (const number of numbers) {
+//   if (number < smallestNumber) {
+//     smallestNumber = number;
+//   }
+// }
+
+// console.log("smallestNumber: ", smallestNumber);
+
+// let biggestNumber = numbers[0];
+
+// for (const number of numbers) {
+//   if (number > biggestNumber) {
+//     biggestNumber = number;
+//   }
+// }
+
+// console.log("biggestNumber: ", biggestNumber);
